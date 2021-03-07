61e50edb7a8eb32d8377db5eda8926d9cac705c8
unknown <younews372@gmail.com>
2021-03-08 01:19:48 +0700

    sampai autentifkasi email

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,13 @@ Route::get('/checkout/success', 'CheckoutController@success')
 
 Route::prefix('admin')
      ->namespace('Admin')
+     ->middleware(['auth', 'admin'])
      ->group(function() {
         Route::get('/', 'DashboardController@index')
           ->name('dashboard');
      });
+
+
+     // verifikasi email
+Auth::routes(['verify' => true]);
 
