8b6045d841f8ca70003a101783ed20b48483856c
unknown <younews372@gmail.com>
2021-04-03 01:52:44 +0700

    crud gallery

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::prefix('admin')
           ->name('dashboard');
 
        Route::resource('travel-package', 'TravelPackageController');
+       Route::resource('gallery', 'GalleryController');
      });
 
 
